905620239f77c5cec1c489569bd4742259721d1a
thakre49 <ritikthakre49@gmail.com>
2025-08-17 15:56:23 +0530

    a >= b >= c  ---> a >= c

---
+++ 1300/B_Most_socially_distanced_subsequence.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Aliases
+using ll = long long;
+using lld = long double;
+using ull = unsigned long long;
+
+// Constants
+const lld pi = 3.141592653589793238;
+const ll mod = 1e9 + 7;
+
+// TypeDEf
+typedef pair<int, int> pri;
+typedef unordered_map<int, int> umi;
+typedef map<int, int> mi;
+
+// Macros
+#define pb push_back
+#define br cout << "\n"
+//****************************************************************
+/*
+
+0 -> Neutral
+1 -> Increasing
+2 -> Decreasing
+
+*/
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++)
+        cin >> nums[i];
+
+    int current = 0;
+    vector<int> ans;
+
+    ans.push_back(nums[0]);
+
+    int ind = 0;
+
+    while (ind < n - 1)
+    {
+        if (nums[ind] == nums[ind + 1])
+        {
+            ind++;
+            continue;
+        }
+        int now = nums[ind] > nums[ind + 1] ? 2 : 1;
+
+        if (now == current || current == 0)
+        {
+            current = now;
+            ind++;
+        }
+        else
+        {
+            ans.push_back(nums[ind]);
+            ind++;
+            current = now;
+        }
+    }
+
+    ans.push_back(nums[n - 1]);
+    cout << ans.size();
+    br;
+    for (auto num : ans)
+    {
+        cout << num << " ";
+    }
+    br;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+        solve();
+    }
+
+    return 0;
+}
