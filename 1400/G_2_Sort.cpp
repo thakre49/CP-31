fca57cd2587682707c16efe5ebca8873d4826e40
thakre49 <ritikthakre49@gmail.com>
2025-09-11 23:33:22 +0530

    Analyze the complex mathematical equation properly

---
+++ 1400/G_2_Sort.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Aliases
+using ll = long long;
+using lld = long double;
+using ull = unsigned long long;
+
+// Constants
+const lld pi = 3.141592653589793238;
+const ll mod = 1e9 + 7;
+
+// TypeDEf
+typedef pair<int, int> pri;
+typedef unordered_map<int, int> umi;
+typedef map<int, int> mi;
+
+// Macros
+#define pb push_back
+#define br cout << "\n"
+//****************************************************************
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++)
+        cin >> nums[i];
+    int ans = 0;
+    int count = 1;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (2 * nums[i] > nums[i - 1])
+            count++;
+        else
+        {
+            ans += max(0, count - k);
+            count = 1;
+        }
+    }
+    ans += max(0, count - k);
+    cout << ans;
+    br;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+        solve();
+    }
+
+    return 0;
+}
